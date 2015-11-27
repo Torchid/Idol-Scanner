c1f62133e140f4bb56dceb891178ddb51ff1dc1c
Torchid <rachel.l.popa@gmail.com>
2015-11-26 21:44:22 -0600

    Add ability to scan for NFC tags

--- app/src/main/java/com/direyorkie/idolscanner/ScannerActivity.java
+++ app/src/main/java/com/direyorkie/idolscanner/ScannerActivity.java
@@ -1,13 +1,28 @@
 package com.direyorkie.idolscanner;
 
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NfcAdapter;
+import android.nfc.tech.NfcF;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.widget.TextView;
 
 public class ScannerActivity extends AppCompatActivity {
 
     private final String INTENT_MSG = "123";
+    private final String TAG = AppCompatActivity.class.getSimpleName();
     private int idolID;
+
+    private NfcAdapter mAdapter;
+    private PendingIntent mPendingIntent;
+    private IntentFilter[] mFilters;
+    private String[][] mTechLists;
+    private TextView mText;
+    private int mCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +32,51 @@ public class ScannerActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mText = (TextView) findViewById(R.id.text);
+        mText.setText("Scan a tag");
+
+        mAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        // Create a generic PendingIntent that will be deliver to this activity. The NFC stack
+        // will fill in the intent with the details of the discovered tag before delivering to
+        // this activity.
+        mPendingIntent = PendingIntent.getActivity(
+                this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+
+        //Tag tagFromIntent = pendingIntent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+
+        //Setup an intent filter for all MIME based dispatches
+        IntentFilter ndef = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        try {
+            ndef.addDataType("*/*");    // Handles all MIME based dispatches.
+        }
+        catch (IntentFilter.MalformedMimeTypeException e) {
+            throw new RuntimeException("fail", e);
+        }
+        mFilters = new IntentFilter[] {ndef, };
+
+        mTechLists = new String[][] { new String[] { NfcF.class.getName() } };
+
+       // readTag();
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mAdapter != null) {
+            mAdapter.enableForegroundDispatch(this, mPendingIntent, mFilters, mTechLists);
+        }
+    }
+    @Override
+    public void onNewIntent(Intent intent) {
+        Log.i("Foreground dispatch", "Discovered tag with intent: " + intent);
+        mText.setText("Discovered tag " + ++mCount + " with intent: " + intent);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mAdapter != null) mAdapter.disableForegroundDispatch(this);
+    }
 }
